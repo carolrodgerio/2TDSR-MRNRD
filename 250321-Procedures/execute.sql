f25a3e1c8cfbd5eb76537894778ffc05caa85473
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-03-21 22:33:45 -0300

    // create execute.sql

---
+++ 250321-Procedures/execute.sql
@@ -0,0 +1,9 @@
+EXEC prd_insert_pais(102, 'Argentina');
+
+EXECUTE prd_insert_pais(102, 'Argentina');
+
+CALL prd_insert_pais(102, 'Argentina');
+
+BEGIN
+    prd_insert_pais(102, 'Argentina');
+END;

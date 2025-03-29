76b5c4b3bd164d3941e2d60dd8117e441d241a1b
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-03-28 21:49:25 -0300

    // create ex1.sql

---
+++ 250328-Exercícios_Procedures/ex1.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE PROCEDURE listarpedidoclientes (
+    p_cod_cliente NUMBER
+) AS
+BEGIN
+    FOR x IN (
+        SELECT
+            cod_cliente      "Código do cliente",
+            dat_pedido       "Data do pedido",
+            val_total_pedido "Valor total do pedido"
+        FROM
+            pedido
+        WHERE
+            cod_cliente = p_cod_cliente
+    ) LOOP
+        dbms_output.put_line(x."Código do cliente");
+    END LOOP;
+END;

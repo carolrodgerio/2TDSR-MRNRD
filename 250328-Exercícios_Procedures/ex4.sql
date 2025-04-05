6c526fc71ff0d9f302e8aba056a6417d60e190df
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-04-04 22:50:31 -0300

    🐛 correção

--- 250328-Exercícios_Procedures/ex4.sql
+++ 250328-Exercícios_Procedures/ex4.sql
@@ -1,6 +1,4 @@
--- EXERCÍCIO 1
-
-CREATE OR REPLACE PROCEDURE insert_produto (
+CREATE OR REPLACE PROCEDURE prc_insere_produto (
     p_cod_produto      NUMBER,
     p_nom_produto      VARCHAR2,
     p_cod_barra        VARCHAR2,
@@ -8,31 +6,26 @@ CREATE OR REPLACE PROCEDURE insert_produto (
     p_dat_cadastro     DATE,
     p_dat_cancelamento DATE
 ) IS
-    exc EXCEPTION;
 BEGIN
-    IF
-        length(p_nom_produto) > 3
-        AND regexp_like(p_nom_produto, '^[0-9]+$')
-    THEN
-        INSERT INTO produto (
-            cod_produto,
-            nom_produto,
-            cod_barra,
-            sta_ativo,
-            dat_cadastro,
-            dat_cancelamento
-        ) VALUES (
-            p_cod_produto,
-            p_nom_produto,
-            p_cod_barra,
-            p_sta_ativo,
-            p_dat_cadastro,
-            p_dat_cancelamento
-        );
-
-        COMMIT;
+    IF length(p_nom_produto) <= 3 OR regexp_like(p_nom_produto, '[0-9]') THEN
+        raise_application_error(-20001, 'Nome do produto inválido: deve ter mais de 3 caracteres e não conter números.');
     END IF;
-EXCEPTION
-    WHEN exc THEN
-        raise_application_error('NOME NÃO VALIDADO');
+
+    INSERT INTO produto (
+        cod_produto,
+        nom_produto,
+        cod_barra,
+        sta_ativo,
+        dat_cadastro,
+        dat_cancelamento
+    ) VALUES (
+        p_cod_produto,
+        p_nom_produto,
+        p_cod_barra,
+        p_sta_ativo,
+        p_dat_cadastro,
+        p_dat_cancelamento
+    );
+
+    COMMIT;
 END;

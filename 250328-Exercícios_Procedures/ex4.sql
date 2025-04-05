a734fb21fbd762ad14bf56739c41ca07bac70c50
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-04-04 22:35:38 -0300

    // create ex4.sql

---
+++ 250328-Exercícios_Procedures/ex4.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE PROCEDURE INSERT_PRODUTO (
+   p_cod_produto NUMBER,
+   p_nom_produto VARCHAR2,
+   p_cod_barra VARCHAR2,
+   p_sta_ativo VARCHAR2,
+   p_dat_cadastro DATE,
+   p_dat_cancelamento DATE) IS
+   exc EXCEPTION;
+BEGIN IF
+    length(p_nom_produto) > 3
+    AND regexp_like(p_nom_produto, '^[0-9]+$')
+THEN
+    INSERT INTO produto (
+        cod_produto,
+        nom_produto,
+        cod_barra,
+        sta_ativo,
+        dat_cadastro,
+        dat_cancelamento
+    ) VALUES (
+        p_cod_produto,
+        p_nom_produto,
+        p_cod_barra,
+        p_sta_ativo,
+        p_dat_cadastro,
+        p_dat_cancelamento
+    );
+
+    COMMIT;
+ELSE EXC THEN EXCEPTION RAISE_APPLICATION_ERROR ( 'NOME NÃO VALIDADO' ) ;
+END IF;
+END;

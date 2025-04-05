cf77935a436e1c8cbbe03e16040b622c7407f827
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-04-04 22:41:00 -0300

    🐛 correção

--- 250328-Exercícios_Procedures/ex4.sql
+++ 250328-Exercícios_Procedures/ex4.sql
@@ -1,32 +1,38 @@
-CREATE OR REPLACE PROCEDURE INSERT_PRODUTO (
-   p_cod_produto NUMBER,
-   p_nom_produto VARCHAR2,
-   p_cod_barra VARCHAR2,
-   p_sta_ativo VARCHAR2,
-   p_dat_cadastro DATE,
-   p_dat_cancelamento DATE) IS
-   exc EXCEPTION;
-BEGIN IF
-    length(p_nom_produto) > 3
-    AND regexp_like(p_nom_produto, '^[0-9]+$')
-THEN
-    INSERT INTO produto (
-        cod_produto,
-        nom_produto,
-        cod_barra,
-        sta_ativo,
-        dat_cadastro,
-        dat_cancelamento
-    ) VALUES (
-        p_cod_produto,
-        p_nom_produto,
-        p_cod_barra,
-        p_sta_ativo,
-        p_dat_cadastro,
-        p_dat_cancelamento
-    );
+-- EXERCÍCIO 1
 
-    COMMIT;
-ELSE EXC THEN EXCEPTION RAISE_APPLICATION_ERROR ( 'NOME NÃO VALIDADO' ) ;
-END IF;
+CREATE OR REPLACE PROCEDURE insert_produto (
+    p_cod_produto      NUMBER,
+    p_nom_produto      VARCHAR2,
+    p_cod_barra        VARCHAR2,
+    p_sta_ativo        VARCHAR2,
+    p_dat_cadastro     DATE,
+    p_dat_cancelamento DATE
+) IS
+    exc EXCEPTION;
+BEGIN
+    IF
+        length(p_nom_produto) > 3
+        AND regexp_like(p_nom_produto, '^[0-9]+$')
+    THEN
+        INSERT INTO produto (
+            cod_produto,
+            nom_produto,
+            cod_barra,
+            sta_ativo,
+            dat_cadastro,
+            dat_cancelamento
+        ) VALUES (
+            p_cod_produto,
+            p_nom_produto,
+            p_cod_barra,
+            p_sta_ativo,
+            p_dat_cadastro,
+            p_dat_cancelamento
+        );
+
+        COMMIT;
+    END IF;
+EXCEPTION
+    WHEN exc THEN
+        raise_application_error('NOME NÃO VALIDADO');
 END;

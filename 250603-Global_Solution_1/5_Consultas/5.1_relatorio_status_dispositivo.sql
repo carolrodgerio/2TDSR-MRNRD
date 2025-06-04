2165eaa3b963eead2c44a0fe16c1c777f0a6309c
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-06-04 18:31:20 -0300

    // create 5.1_relatorio_status_dispositivo.sql

---
+++ 250603-Global_Solution_1/5_Consultas/5.1_relatorio_status_dispositivo.sql
@@ -0,0 +1,34 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    v_id_usuario     NUMBER := 1;
+    CURSOR c_dispositivos IS
+    SELECT
+        d.id_dispositivo,
+        d.status
+    FROM
+        dispositivo d
+    WHERE
+        d.id_usuario = v_id_usuario;
+
+    v_id_dispositivo dispositivo.id_dispositivo%TYPE;
+    v_status         dispositivo.status%TYPE;
+BEGIN
+    dbms_output.put_line('Dispositivos do usuário de ID: ' || v_id_usuario);
+    dbms_output.put_line('--//--');
+    FOR rec IN c_dispositivos LOOP
+        v_id_dispositivo := rec.id_dispositivo;
+        v_status := rec.status;
+        dbms_output.put_line('ID do dispositivo: '
+                             || v_id_dispositivo
+                             || ' | Status: '
+                             || v_status);
+        IF v_status IN ( 'Inativo', 'Stand-by' ) THEN
+            dbms_output.put_line('⚠️  Atenção: Este dispositivo não está operando normalmente.');
+        ELSE
+            dbms_output.put_line('✅ Dispositivo em operação.');
+        END IF;
+
+    END LOOP;
+
+END;

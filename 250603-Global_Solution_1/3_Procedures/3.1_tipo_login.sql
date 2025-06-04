0c5f0c6bcdb900f3aab295cd3642db1ace0e9f4f
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-06-04 16:17:51 -0300

    // create 3.1_tipo_login.sql

---
+++ 250603-Global_Solution_1/3_Procedures/3.1_tipo_login.sql
@@ -0,0 +1,42 @@
+-- inserção
+
+CREATE OR REPLACE PROCEDURE inserir_tipo_login (
+    p_id_tipo NUMBER,
+    p_descricao VARCHAR2
+) AS
+BEGIN
+    INSERT INTO tipo_login VALUES (
+        p_id_tipo, p_descricao
+    );
+END;
+
+-- atualização
+
+CREATE OR REPLACE PROCEDURE atualizar_tipo_login (
+    p_id_tipo NUMBER,
+    p_descricao VARCHAR2
+) AS
+BEGIN
+    UPDATE tipo_login
+    SET descricao = p_descricao
+    WHERE id_tipo = p_id_tipo;
+END;
+
+-- exclusão
+
+CREATE OR REPLACE PROCEDURE excluir_tipo_login (
+    p_id_tipo NUMBER
+) AS
+BEGIN
+    DELETE FROM tipo_login WHERE id_tipo = p_id_tipo;
+END;
+
+-- execução
+
+BEGIN
+    inserir_tipo_login(1, 'ADM');
+    inserir_tipo_login(2, 'Usuário');
+    inserir_tipo_login(3, 'ADM');
+    inserir_tipo_login(4, 'Usuário');
+    inserir_tipo_login(5, 'ADM');
+END;
